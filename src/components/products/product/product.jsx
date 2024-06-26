21d83a7e37e6249d3da8e611a3df958f18fc4584
AmolGaur <b22cs008@iitj.ac.in>
2024-06-26 13:32:57 +0530

    added products shown in home page

---
+++ src/components/products/product/product.jsx
@@ -0,0 +1,19 @@
+import "./product.css";
+
+import prod from "../../images/shirt.png";
+
+const Product = () => {
+    return (
+        <div className="product-card">
+            <div className="thumbnail">
+                <img src={prod} alt="" />
+            </div>
+            <div className="product-details">
+                <span className="name">Product name</span>
+                <span className="price">&#8377;499</span>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/products/products.jsx
@@ -0,0 +1,22 @@
+import "./products.css";
+import Product from "./product/product";
+
+const Products = ({ innerpage, headingText }) => {
+    return (
+        <div className="products-container">
+            {!innerpage && <div className="sec-heading"> { headingText }</div>}
+            <div className="products">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+            </div>
+        </div>
+    );
+};
+
+export default Products;
