491f4fd679dea81dac69a607c981e3f794b67b0e
AmolGaur <b22cs008@iitj.ac.in>
2024-06-26 13:31:38 +0530

    added popular products

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,19 @@
 import "./Home.css";
 import Banner from "./Banner/Banner";
 import Genre from "./Genre/Genre";
+import Products from "../products/products";
 
 
 const Home = () => {
     return (
         <div>
             <Banner />
-            <Genre />
+            <div className="main-content">
+                <div className="layout">
+                    <Genre />
+                    <Products headingText="Popular Products" />
+                </div>
+            </div>
         </div>
     );
 };
