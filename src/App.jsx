17fb0bde0d933227cc01204ff279e1820807a258
AmolGaur <b22cs008@iitj.ac.in>
2024-06-26 14:28:10 +0530

    added different pages in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Signup from "./components/Sign-up/Sign_up";
 import Footer from "./components/Footer/Footer";
+import Category from "./components/Category/category";
+import SingleProduct from "./components/OneProduct/oneproduct";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Signup />
-      <Footer />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                    <Route path = "/" element ={<Home />}/>
+                    <Route path = "/category/:id" element ={<Category />}/>
+                    <Route path = "/product/:id" element ={<SingleProduct />}/>
+            </Routes>
+            <Signup />
+            <Footer />
+        </BrowserRouter>
+    )
 }
 
 export default App;
